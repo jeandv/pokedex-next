acb4e00b978e96165209e9442564752750ea6c7c
Jean Rondon <jeanrdev@gmail.com>
2022-10-08 01:39:38 -0400

    using open graph meta tags

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -7,6 +7,8 @@ interface LayoutProps {
   title?: string;
 }
 
+const origin = (typeof window === 'undefined') ? '' : window.location.origin;
+
 export const Layout: FC<LayoutProps> = ({ children, title }) => {
   return (
     <>
@@ -16,6 +18,10 @@ export const Layout: FC<LayoutProps> = ({ children, title }) => {
         <meta name='description' content='Hola, me llamo Jean Rondón y esto es una pokédex hecha con Next.js + TypeScript. Buscame en mi Linkedin y GitHub como Jeandv.' />
         <meta name='keywords' content='Jeandv, Jeandv pokedex, Jeandv pokemon, Jean pokemon, pokedex' />
         <link rel='icon' href='/meowth.ico' />
+        {/* open graph meta tags */}
+        <meta property='og:title' content={`Información sobre el ${title}`} />
+        <meta property='og:description' content={`Esta es la página donde se muestra la información de ${title}`} />
+        <meta property='og:image' content={`${origin}/img/banner.png`} />
       </Head>
       <NavbarSection />
       <main style={{ padding: '0 30px' }}>
